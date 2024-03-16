6c440702446a18d7b7a559930ab9b027d552357b
Иван <159775506+Smirnovvania@users.noreply.github.com>
2024-03-16 11:41:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+document.getElementById('date_s').valueAsDate = new Date();
+document.getElementById('date_b').valueAsDate = new Date();
+document.getElementById('date_x').valueAsDate = new Date();
+
+
+
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzH83Z_aeiORiTUso4OcigQzswpeJiW2iDOgZKP7TjNqSbC3cOPy5-HJHQ6qq5ohZex/exec'
 
 $('.tab').click(function(event) {
@@ -34,4 +41,3 @@ form3.addEventListener('submit', e => {
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Ошибка', error.message))
 })
-     
